4a3a7a53d3a315f4efca334faa1903bc750ac271
TaftSound <mattmurphytattoos@gmail.com>
2023-03-10 13:04:27 -0800

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React, { useContext } from 'react';
+import React from 'react';
 import ShopPage from './components/shop-page';
 import Homepage from './components/homepage';
 import NavBar from './components/navbar';
 
-// components: NavBar, Homepage, ShoppingCart
-// two routes: Homepage and ShoppingCart
-
-// navbar on both pages, in this App.js file we are only going to
-// set up the routing and create the context to pass through the app
-
-// use "useContext" for practice
-
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename='/shopping-cart'>
         <NavBar />
         <Routes>
-          <Route path='/' element={<Homepage />} />
+          <Route exact path='/' element={<Homepage />} />
           <Route path='/shop' element={<ShopPage />} />
         </Routes>
       </BrowserRouter>
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -9,10 +9,6 @@ function Homepage () {
       <div className="background-container">
         <img className="background-image" src={homepageImage} alt="Racks of clothing" />
       </div>
-      {/* <div className="text-card">
-        <p>Welcome to our online clothing store, where fashion meets
-        comfort!</p>
-      </div> */}
     </div>
   )
 }
